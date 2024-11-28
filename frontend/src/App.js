6e7de515f1b2427a6db3542eb79d586a2609ae41
taovietduc193 <95725645+taovietduc193@users.noreply.github.com>
2024-11-29 03:40:22 +0700

    update code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from './router/admin';
-import Navbar from './components/Navbar';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div className="app min-h-screen">
-        <div className="fixed top-0 left-0 right-0 z-50">
-          <Navbar />
-        </div>
-        <div className="pt-14 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
-          <div className="w-full">
-            <Routes>
-              {publicRoutes.map((route, index) => {
-                const Page = route.component;
-                return <Route key={index} path={route.path} element={<Page />} />;
-              })}
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/router/admin.jsx
+++ frontend/src/router/admin.jsx
@@ -1,16 +1,12 @@
 import Home from '../page/user/Home';
-import Reels from '../page/user/Reels';
 import Group from '../page/user/Group';
-import { Notification } from '../page/user/Notification';
 import { Profile } from '../page/user/Profile';
 import { Login } from '../page/user/Login';
 
 // Public routes
 const publicRoutes = [
     { path: '/', component: Home },
-    { path: '/reels', component: Reels },
     { path: '/group', component: Group }, 
-    { path: '/notification', component: Notification },
     { path: '/profile', component: Profile },
     { path: '/login', component: Login },
 ];
